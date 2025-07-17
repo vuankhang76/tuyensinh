00e6b18f1f64e8a177262a924d46c3458059febb
Vũ An Khang <mrkhang957@gmail.com>
2025-07-17 18:53:48 +0700

    Update axios.js
    Hot fix axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  // baseURL: 'https://tuyensinh-infor.up.railway.app/api',
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://tuyensinh-infor.up.railway.app/api',
   timeout: 60000,
   headers: {
     'Content-Type': 'application/json',
